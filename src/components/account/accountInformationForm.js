33f751b8c9c54ed332a69d7e88b44e1c81f7b1b8
annasmbr <donanna77@hotmail.com>
2020-12-29 00:35:47 +0100

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -84,6 +84,8 @@ class AccountInformationForm extends Component {
                 name='change-password'
                 component={LongGrayButton}/>*/}
 
+           <div className='account-information-form__line'></div>
+
                {
                     this.state.showPasswords ?
                         [
@@ -104,7 +106,20 @@ class AccountInformationForm extends Component {
                             title='Confirm Password'
                             placeholder='Confirm Password'
                             name='confirm'
-                            component={FormInput}/>
+                            component={FormInput}/>,
+                            <Field key={3} className='account-information-form__update-information'
+                            onClick={() => this.setState({ showPasswords: false })}
+                            type='submit'
+                            title='Update Information'
+                            name='update-information'
+                            component={FormButton}/>,
+                            <Field key={4} className='account-information-form__cancel'
+                            onClick={() => this.setState({ showPasswords: false })}
+                            type='button'
+                            title='Cancel'
+                            name='cancel'
+                            short={true}
+                            component={FormButton}/>
                         ]
 
                     :
