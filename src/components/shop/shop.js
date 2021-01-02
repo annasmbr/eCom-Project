23efcaaff8a38313e71b07a0e3cd234e28a7b0ee
annasmbr <donanna77@hotmail.com>
2021-01-02 23:26:11 +0100

    filter products with query action creator

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -38,7 +38,8 @@ class Shop extends Component {
     }
 
     onSubmit = (fields) => {
-        console.log(fields);
+        //console.log(fields);
+        this.props.filterProductsWithQuery(fields)
     }
  
     render() {
