6003c1d7ed40e0309d9331bcd3459c7f00dae24f
annasmbr <donanna77@hotmail.com>
2021-01-06 20:44:58 +0100

    added shop-cart component and style sheet for that component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            showCart: true
+        }
+    }
 
     componentDidMount() {
         const headerLinks = [
@@ -44,6 +53,7 @@ class Shop extends Component {
     }
  
     render() {
+        return <ShopCart className='shop__cart'/>
 
         return (
             <div className='shop'>
@@ -68,6 +78,9 @@ class Shop extends Component {
                         })
                     }
                 </div>
+                {
+                    this.state.showCart ? <ShopCart className='shop__cart' /> : ''
+                }
                 {/* shop cart button */}
             </div>
         )
